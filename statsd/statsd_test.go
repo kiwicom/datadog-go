f5ce1c08a6dc6f2ef3a156bfd85458561f62afbd
Maxime mouial <maxime.mouial@gmail.com>
2022-02-08 16:08:10 +0100

    Export internal error
    This allows users to check the error type when sending message that can't fit in a buffer.

--- statsd/statsd_test.go
+++ statsd/statsd_test.go
@@ -330,3 +330,15 @@ func Test_isOriginDetectionEnabled(t *testing.T) {
 		})
 	}
 }
+
+func TestMessageTooLongError(t *testing.T) {
+	client, err := New("localhost:8765", WithMaxBytesPerPayload(10), WithoutClientSideAggregation())
+	require.NoError(t, err)
+
+	err = client.Gauge("fake_name_", 21, nil, 1)
+	require.Error(t, err)
+	assert.Equal(t, "message too long. See 'WithMaxBytesPerPayload' documentation.", err.Error())
+
+	_, ok := err.(*MessageTooLongError)
+	assert.True(t, ok)
+}
